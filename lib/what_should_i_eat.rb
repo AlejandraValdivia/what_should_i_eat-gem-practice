0512552bd943ce3cd16279889e6e2d6170d82ffd
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2021-03-26 15:08:57 -0700

    Updated what_should_i_eat.rb

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -2,16 +2,16 @@
 
 require 'what_should_i_eat/printing'
 require 'http'
-require 'json'
+require 'JSON'
  
 module WhatShouldIEat
-  def WhatShouldIEat.fresh
+
     result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
     recipes = JSON.parse(result)
     name = recipes.first['items'].first['label']
     url = recipes.first['items'].first['url']
     Printing.print name, url
-  end
+
 end
 
 
